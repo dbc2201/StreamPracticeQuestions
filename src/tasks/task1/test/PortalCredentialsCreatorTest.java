189464680fa3d0b637037480166e8220faaee28f
dbc2201 <divyanshb22@gmail.com>
2022-03-13 04:26:17 +0530

    ass JUnit to project, add a test class for task1

---
+++ src/tasks/task1/test/PortalCredentialsCreatorTest.java
@@ -0,0 +1,16 @@
+package tasks.task1.test;
+
+class PortalCredentialsCreatorTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void createListOfUserNames() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void createListOfRandomPasswords() {
+    }
+}
