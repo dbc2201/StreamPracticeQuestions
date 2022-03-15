dbd1b9b75a310fad38943beb05d7d439dd31e8f2
dbc2201 <divyanshb22@gmail.com>
2022-03-15 06:46:15 +0530

    task1; write test case 1

--- src/tasks/task1/test/PortalCredentialsCreatorTest.java
+++ src/tasks/task1/test/PortalCredentialsCreatorTest.java
@@ -1,16 +1,37 @@
 package tasks.task1.test;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import tasks.task1.PortalCredentialsCreator;
+import tasks.task1.Student;
+import tasks.task1.UserName;
+
+import java.util.List;
+
 class PortalCredentialsCreatorTest {
 
-    @org.junit.jupiter.api.BeforeEach
+    private PortalCredentialsCreator creator;
+
+    @BeforeEach
     void setUp() {
+        creator = new PortalCredentialsCreator();
     }
 
-    @org.junit.jupiter.api.Test
-    void createListOfUserNames() {
+    @Test
+    void createEmptyListOfUserNames() {
+        // create an empty list of usernames
+        List<UserName> userNames = List.of();
+        // mark this empty list as the expected value
+        var expected = userNames;
+        // create an empty list of students
+        List<Student> students = List.of();
+        // use this empty list to call the method being tested
+        var actual = creator.createListOfUserNames(students);
+        Assertions.assertEquals(expected, actual, "Case #1: method should be able to create empty lists.");
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void createListOfRandomPasswords() {
     }
 }
