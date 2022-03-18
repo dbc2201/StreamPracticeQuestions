464ca969d2fd15b92c6506999dabca9cd5a79e40
dbc2201 <divyanshb22@gmail.com>
2022-03-18 18:35:41 +0530

    task; add getAllOldPatients() method

--- src/tasks/task9/code/PatientHelper.java
+++ src/tasks/task9/code/PatientHelper.java
@@ -1,4 +1,10 @@
 package tasks.task9.code;
 
+import java.util.List;
+import java.util.Set;
+
 public class PatientHelper {
+    public Set<Patient> getAllOldPatients(List<Patient> patients) {
+        return Set.of();
+    }
 }
