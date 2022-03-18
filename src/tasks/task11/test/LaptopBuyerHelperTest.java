c0c50bfa95d7e76ef0cee8e36cef7d782adf6501
dbc2201 <divyanshb22@gmail.com>
2022-03-18 18:17:45 +0530

    task11; add test class

---
+++ src/tasks/task11/test/LaptopBuyerHelperTest.java
@@ -0,0 +1,15 @@
+package tasks.task11.test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class LaptopBuyerHelperTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
