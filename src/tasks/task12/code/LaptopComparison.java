bfcf2a823a6c325c27afb6eebf20b4f3cebd76fa
dbc2201 <divyanshb22@gmail.com>
2022-03-18 18:43:49 +0530

    task12; add getGamingLaptops() method

--- src/tasks/task12/code/LaptopComparison.java
+++ src/tasks/task12/code/LaptopComparison.java
@@ -1,4 +1,10 @@
 package tasks.task12.code;
 
+import java.util.List;
+import java.util.Set;
+
 public class LaptopComparison {
+    public Set<Laptop> getGamingLaptops(List<Laptop> laptops, byte ramCapacity, byte graphicsCardCapacity) {
+        return Set.of();
+    }
 }
