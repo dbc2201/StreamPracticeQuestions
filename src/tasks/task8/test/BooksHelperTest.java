2a10a3c97a76cdd0acad66152729cc6fdb4005a7
dbc2201 <divyanshb22@gmail.com>
2022-03-18 18:14:18 +0530

    task8; add test class

---
+++ src/tasks/task8/test/BooksHelperTest.java
@@ -0,0 +1,15 @@
+package tasks.task8.test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class BooksHelperTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
