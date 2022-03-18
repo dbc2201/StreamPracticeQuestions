948e93d42efcc421a3bea2399a003f3b0d234539
dbc2201 <divyanshb22@gmail.com>
2022-03-18 18:42:59 +0530

    task11; add findLaptop() method

--- src/tasks/task11/code/LaptopBuyerHelper.java
+++ src/tasks/task11/code/LaptopBuyerHelper.java
@@ -1,11 +1,14 @@
 package tasks.task11.code;
 
 import java.util.List;
+import java.util.Set;
 
 public class LaptopBuyerHelper {
     public List<Laptop> getLaptopList(List<Laptop> laptops, byte ramCapacity) {
         return List.of();
     }
 
-
+    public Set<Laptop> findLaptop(List<Laptop> laptops, byte ramCapacity, double price) {
+        return Set.of();
+    }
 }
