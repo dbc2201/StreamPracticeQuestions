41c8919998a0b02cdb5f8577bc91ed94897088af
dbc2201 <divyanshb22@gmail.com>
2022-03-18 18:15:33 +0530

    task9; add test class

---
+++ src/tasks/task9/test/PatientHelperTest.java
@@ -0,0 +1,15 @@
+package tasks.task9.test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class PatientHelperTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
