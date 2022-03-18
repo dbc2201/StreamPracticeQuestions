4e82009e635f5aa4779c7c5506d8590d60dbf5a7
dbc2201 <divyanshb22@gmail.com>
2022-03-18 18:06:48 +0530

    task4; add test class

---
+++ src/tasks/task4/test/VideoAnalyticsTest.java
@@ -0,0 +1,15 @@
+package tasks.task4.test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class VideoAnalyticsTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
