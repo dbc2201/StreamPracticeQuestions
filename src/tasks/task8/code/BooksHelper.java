7047f2db510351c24eabfe74209bbbca9dc56cf7
dbc2201 <divyanshb22@gmail.com>
2022-03-18 18:32:56 +0530

    task7; add findAllBooksOnSubject() method

--- src/tasks/task8/code/BooksHelper.java
+++ src/tasks/task8/code/BooksHelper.java
@@ -1,4 +1,11 @@
 package tasks.task8.code;
 
+import java.util.List;
+import java.util.Set;
+
 public class BooksHelper {
+    public List<Book> findAllBooksOnSubject(Set<Book> books, String subject) {
+        return List.of();
+    }
+
 }
