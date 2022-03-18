5ccb9173f6e2d3c12c01aaafa0d212c531804e7a
dbc2201 <divyanshb22@gmail.com>
2022-03-18 18:12:35 +0530

    task7; add test class

---
+++ src/tasks/task7/test/CollegeLibraryTest.java
@@ -0,0 +1,15 @@
+package tasks.task7.test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class CollegeLibraryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
