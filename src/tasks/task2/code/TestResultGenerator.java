ab98550b0405840c93e9f1f796c16013315bcb8f
dbc2201 <divyanshb22@gmail.com>
2022-03-18 18:21:56 +0530

    task2; add getDetailsOfFailedStudents() method signature

--- src/tasks/task2/code/TestResultGenerator.java
+++ src/tasks/task2/code/TestResultGenerator.java
@@ -1,4 +1,10 @@
 package tasks.task2.code;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class TestResultGenerator {
+    public HashSet<String> getDetailsOfFailedStudents(Set<Student> studentSet) {
+        return new HashSet<>();
+    }
 }
