0f304909e1368e2dbaaff987e7b701816f1aa4f1
dbc2201 <divyanshb22@gmail.com>
2022-03-18 18:20:38 +0530

    task13; add test class

---
+++ src/tasks/task13/test/TestGeneratorTest.java
@@ -0,0 +1,15 @@
+package tasks.task13.test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class TestGeneratorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
