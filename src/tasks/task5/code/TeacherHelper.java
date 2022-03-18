07fd9dd01ce83c8ed6f0d2ca023f7c2ed139d626
dbc2201 <divyanshb22@gmail.com>
2022-03-18 18:27:26 +0530

    task5; add getBusyTeachers() method

--- src/tasks/task5/code/TeacherHelper.java
+++ src/tasks/task5/code/TeacherHelper.java
@@ -1,10 +1,14 @@
 package tasks.task5.code;
 
 import java.util.List;
+import java.util.Set;
 
 public class TeacherHelper {
     public List<Teacher> findSubjectTeacher(List<Teacher> allTeachers, String subjectName) {
         return List.of();
     }
 
+    public Set<Teacher> getBusyTeachers(List<Teacher> allTeachers) {
+        return Set.of();
+    }
 }
