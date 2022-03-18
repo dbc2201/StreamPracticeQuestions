84ca6d75d95b85a437f3b616aeb9640bb31aa4c2
dbc2201 <divyanshb22@gmail.com>
2022-03-18 18:23:25 +0530

    task2; add test methods for task2

--- src/tasks/task2/test/TestResultGeneratorTest.java
+++ src/tasks/task2/test/TestResultGeneratorTest.java
@@ -2,6 +2,7 @@ package tasks.task2.test;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class TestResultGeneratorTest {
 
@@ -11,5 +12,13 @@ class TestResultGeneratorTest {
 
     @AfterEach
     void tearDown() {
+    }
+
+    @Test
+    void getDetailsOfFailedStudents() {
+    }
+
+    @Test
+    void sortStudentsForRanking() {
     }
 }
