eaecc718ee6e9f6fa9c65f54110b5f1707e7ae60
dbc2201 <divyanshb22@gmail.com>
2022-03-18 18:45:27 +0530

    task13; add createTest() method

--- src/tasks/task13/code/TestGenerator.java
+++ src/tasks/task13/code/TestGenerator.java
@@ -7,4 +7,8 @@ public class TestGenerator {
     public HashSet<Question> fetchQuestionList(List<Question> allQuestions, byte marks) {
         return new HashSet<>();
     }
+
+    public HashSet<Question> createTest(List<Question> allQuestions) {
+        return new HashSet<>();
+    }
 }
