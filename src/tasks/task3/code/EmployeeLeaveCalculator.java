4cc030fab1deca693ab4239321945f41921113a3
dbc2201 <divyanshb22@gmail.com>
2022-03-18 18:24:30 +0530

    task3; add getNumberOfLeaves()

--- src/tasks/task3/code/EmployeeLeaveCalculator.java
+++ src/tasks/task3/code/EmployeeLeaveCalculator.java
@@ -1,10 +1,15 @@
 package tasks.task3.code;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeeLeaveCalculator {
     public List<Short> getDefaulterEmployeeIDList(List<Employee> employeeList) {
         return List.of();
+    }
+
+    public ArrayList<String> getNumberOfLeaves(List<Employee> employeeList) {
+        return new ArrayList<>();
     }
 
 }
