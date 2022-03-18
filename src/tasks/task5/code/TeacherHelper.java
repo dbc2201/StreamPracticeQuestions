3fb199a6a650c3cf333dd85df64de2f9cd001a89
dbc2201 <divyanshb22@gmail.com>
2022-03-18 18:26:57 +0530

    task5; add findSubjectTeacher() method

--- src/tasks/task5/code/TeacherHelper.java
+++ src/tasks/task5/code/TeacherHelper.java
@@ -1,4 +1,10 @@
 package tasks.task5.code;
 
+import java.util.List;
+
 public class TeacherHelper {
+    public List<Teacher> findSubjectTeacher(List<Teacher> allTeachers, String subjectName) {
+        return List.of();
+    }
+
 }
