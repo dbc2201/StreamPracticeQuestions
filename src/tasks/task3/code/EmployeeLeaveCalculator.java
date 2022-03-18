d8c040d73a7d9ec6090e4ea56f3d942ef1c6b003
dbc2201 <divyanshb22@gmail.com>
2022-03-18 18:24:05 +0530

    task3; add getDefaulterEmployeeIDList()

--- src/tasks/task3/code/EmployeeLeaveCalculator.java
+++ src/tasks/task3/code/EmployeeLeaveCalculator.java
@@ -1,4 +1,10 @@
 package tasks.task3.code;
 
+import java.util.List;
+
 public class EmployeeLeaveCalculator {
+    public List<Short> getDefaulterEmployeeIDList(List<Employee> employeeList) {
+        return List.of();
+    }
+
 }
