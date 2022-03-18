8b68a302db9786ce850761d6c11fea02c536df7e
dbc2201 <divyanshb22@gmail.com>
2022-03-18 18:04:15 +0530

    task3; add test class

---
+++ src/tasks/task3/test/EmployeeLeaveCalculatorTest.java
@@ -0,0 +1,15 @@
+package tasks.task3.test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class EmployeeLeaveCalculatorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
