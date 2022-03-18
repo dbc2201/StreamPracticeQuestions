c9e5c245d7dfd136232560cf3407ad6432657e3a
dbc2201 <divyanshb22@gmail.com>
2022-03-18 18:19:13 +0530

    task12; add test class

---
+++ src/tasks/task12/test/LaptopComparisonTest.java
@@ -0,0 +1,15 @@
+package tasks.task12.test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class LaptopComparisonTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
