4ddbd633d2913f05d0288b983bd1b0834a91db86
dbc2201 <divyanshb22@gmail.com>
2022-03-18 18:09:22 +0530

    task5; add test class

---
+++ src/tasks/task5/test/TeacherHelperTest.java
@@ -0,0 +1,15 @@
+package tasks.task5.test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class TeacherHelperTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
