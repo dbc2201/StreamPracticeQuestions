0b184354f51d57bdc295775a2504330abb08cee7
dbc2201 <divyanshb22@gmail.com>
2022-03-18 18:25:56 +0530

    task4; add showDetailsOfShortVideos() method

--- src/tasks/task4/code/VideoAnalytics.java
+++ src/tasks/task4/code/VideoAnalytics.java
@@ -2,9 +2,14 @@ package tasks.task4.code;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class VideoAnalytics {
     public HashSet<YouTubeVideo> getTopTenViralVideos(List<YouTubeVideo> videos) {
         return new HashSet<>();
+    }
+
+    public List<String> showDetailsOfShortVideos(Set<YouTubeVideo> videos) {
+        return List.of();
     }
 }
