cdf5d8ddb979e2e9de14941170e685daac121da8
dbc2201 <divyanshb22@gmail.com>
2022-03-18 18:25:36 +0530

    task4; add getTopTenViralVideos() method

--- src/tasks/task4/code/VideoAnalytics.java
+++ src/tasks/task4/code/VideoAnalytics.java
@@ -1,4 +1,10 @@
 package tasks.task4.code;
 
+import java.util.HashSet;
+import java.util.List;
+
 public class VideoAnalytics {
+    public HashSet<YouTubeVideo> getTopTenViralVideos(List<YouTubeVideo> videos) {
+        return new HashSet<>();
+    }
 }
