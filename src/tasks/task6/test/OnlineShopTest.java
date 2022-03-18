403292f8c943d4faede66689d625c31fbf819e09
dbc2201 <divyanshb22@gmail.com>
2022-03-18 18:11:00 +0530

    task6; add test class

---
+++ src/tasks/task6/test/OnlineShopTest.java
@@ -0,0 +1,15 @@
+package tasks.task6.test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class OnlineShopTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
