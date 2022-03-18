089dad51c2951bb4088f81c8d57763efa26e34e2
dbc2201 <divyanshb22@gmail.com>
2022-03-18 18:44:08 +0530

    task12; add sortGamingLaptops() method

--- src/tasks/task12/code/LaptopComparison.java
+++ src/tasks/task12/code/LaptopComparison.java
@@ -7,4 +7,8 @@ public class LaptopComparison {
     public Set<Laptop> getGamingLaptops(List<Laptop> laptops, byte ramCapacity, byte graphicsCardCapacity) {
         return Set.of();
     }
+
+    public Set<Laptop> sortGamingLaptops(Set<Laptop> laptops) {
+        return Set.of();
+    }
 }
