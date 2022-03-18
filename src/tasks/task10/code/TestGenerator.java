dfa99fd7b8cf3ceb2a87f5774202b2c2d44cf54b
dbc2201 <divyanshb22@gmail.com>
2022-03-18 18:40:38 +0530

    task10; add getAllPositivePatients() methods

--- src/tasks/task10/code/TestGenerator.java
+++ src/tasks/task10/code/TestGenerator.java
@@ -1,4 +1,10 @@
 package tasks.task10.code;
 
+import java.util.List;
+import java.util.Set;
+
 public class TestGenerator {
+    public Set<Patient> getAllPositivePatients(List<Patient> allPatients) {
+        return Set.of();
+    }
 }
