0801320ef1f953ca67cbd8313a1238c4befcebe5
dbc2201 <divyanshb22@gmail.com>
2022-03-18 18:29:15 +0530

    task6; add test methods

--- src/tasks/task6/test/OnlineShopTest.java
+++ src/tasks/task6/test/OnlineShopTest.java
@@ -2,6 +2,7 @@ package tasks.task6.test;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class OnlineShopTest {
 
@@ -11,5 +12,13 @@ class OnlineShopTest {
 
     @AfterEach
     void tearDown() {
+    }
+
+    @Test
+    void getListOfSmartPhones() {
+    }
+
+    @Test
+    void findBudgetSmartPhones() {
     }
 }
